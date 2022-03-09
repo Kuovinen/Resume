bbbadf012d0c4f826bac81c5d248e5e742012739
RomanZujev <roman.zujev@gmail.com>
2022-03-09 19:39:05 +0200

    added state handling for CONTACT and RESUME sections

--- src/App.js
+++ src/App.js
@@ -3,22 +3,45 @@ import React, { useState } from "react";
 import Main from "./components/Main.js";
 import Header from "./components/Header.js";
 import Resume from "./components/Resume.js";
+import Contact from "./components/Contact.js";
 import scrollDown from "./components/handlers.js";
 
 function App() {
-  let [resumeVisibility, setResumeVisibility] = useState("block");
+  let [resumeVisibility, setResumeVisibility] = useState("none");
   function toggleResume() {
-    console.log("clickedResume");
-    resumeVisibility === "block"
-      ? setResumeVisibility("none")
-      : setResumeVisibility("block");
+    //Hide RESUME if shown. Otherwise show RESUME, but also close CONTACT.
+    if (resumeVisibility === "block") {
+      setResumeVisibility("none");
+    } else {
+      if (contactVisibility === "grid") {
+        setContactVisibility("none");
+      }
+      setResumeVisibility("block");
+    }
+  }
+  let [contactVisibility, setContactVisibility] = useState("none");
+  function toggleContact() {
+    //Hide CONTACT if shown. Otherwise show CONTAT, but also close RESUME.
+    if (contactVisibility === "grid") {
+      setContactVisibility("none");
+    } else {
+      if (resumeVisibility === "block") {
+        setResumeVisibility("none");
+      }
+      setContactVisibility("grid");
+    }
   }
 
   return (
     <div className="App">
       <br />
-      <Header key="header" resumeHandler={toggleResume} />
+      <Header
+        key="header"
+        resumeHandler={toggleResume}
+        contactHandler={toggleContact}
+      />
       <Resume handler={toggleResume} visibility={resumeVisibility} />
+      <Contact handler={toggleContact} visibility={contactVisibility} />
       <br />
       <div className="arrow" onClick={scrollDown}></div>
       <br />
