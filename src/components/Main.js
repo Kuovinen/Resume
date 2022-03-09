91ca994bf5296f1b00b4e9d96de06ff47b7087d5
RomanZujev <roman.zujev@gmail.com>
2022-03-09 15:35:30 +0200

    added imaging data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,38 @@
 import Project from "./Project.js";
 
+import restaScreenshot from "../restaurantScreen.jpg";
+import chatAppScreen from "../chatAppScreen.jpg";
+import cookbookDBScreen from "../cookbookDBScreen.jpg";
+import movieDBScreen from "../movieDBScreen.jpg";
+import taskManagerScreen from "../taskManagerScreen.jpg";
 const projects = [
-  <Project name="RESTAURANT" />,
-  <Project name="TASK MANAGER" />,
-  <Project name="MOVIE DATABASE" />,
-  <Project name="COOCKBOOK" />,
-  <Project name="CHATAPP" />,
-  <Project name="MOVIE DATABASE" />,
+  <Project
+    key="p1"
+    name="FAKE RESTAURANT"
+    class="projectU"
+    img={restaScreenshot}
+  />,
+  <Project
+    key="p2"
+    name="TASK MANAGER"
+    class="projectU"
+    img={taskManagerScreen}
+  />,
+  <Project
+    key="p3"
+    name="MOVIE DATABASE"
+    class="projectU"
+    img={movieDBScreen}
+  />,
+  <Project key="p4" name="COOCKBOOK" class="projectU" img={cookbookDBScreen} />,
+  <Project key="p5" name="CHATAPP" class="project" img={chatAppScreen} />,
+  <Project key="p6" name="API" class="projectU" img={restaScreenshot} />,
 ];
 
 export default function Main() {
   return (
     <div id="main">
-      <span>PROJECTS</span>
+      <span id="projectLogo">PROJECTS</span>
       <br />
       <div className="projectsContainer">{projects}</div>
     </div>
