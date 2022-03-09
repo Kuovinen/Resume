4f3b48647fbce7347155b663a355892921329d21
RomanZujev <roman.zujev@gmail.com>
2022-03-09 15:33:26 +0200

    added sate for resume button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import React, { useState } from "react";
+
 import Main from "./components/Main.js";
 import Header from "./components/Header.js";
+import Resume from "./components/Resume.js";
+import scrollDown from "./components/handlers.js";
 
 function App() {
+  let [resumeVisibility, setResumeVisibility] = useState("block");
+  function toggleResume() {
+    console.log("clickedResume");
+    resumeVisibility === "block"
+      ? setResumeVisibility("none")
+      : setResumeVisibility("block");
+  }
+
   return (
     <div className="App">
       <br />
-      <Header />
+      <Header key="header" resumeHandler={toggleResume} />
+      <Resume handler={toggleResume} visibility={resumeVisibility} />
       <br />
-      <div className="arrow"></div>
+      <div className="arrow" onClick={scrollDown}></div>
       <br />
-      <Main />
+      <Main key="main" />
     </div>
   );
 }
