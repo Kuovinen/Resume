7bf64eaf45554db33930431b0aaa084f10ae0a6f
RomanZujev <roman.zujev@gmail.com>
2022-03-21 19:36:39 +0200

    added links to several projects and git

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,12 +5,14 @@ import chatAppScreen from "../chatAppScreen.jpg";
 import cookbookDBScreen from "../cookbookDBScreen.jpg";
 import movieDBScreen from "../movieDBScreen.jpg";
 import taskManagerScreen from "../taskManagerScreen.jpg";
+import weatherMap from "../weatherMap.jpg";
 const projects = [
   <Project
     key="p1"
     name="FAKE RESTAURANT"
     class="projectU"
     img={restaScreenshot}
+    code="https://github.com/RomanZujev/Restaurant"
   />,
   <Project
     key="p2"
@@ -25,8 +27,20 @@ const projects = [
     img={movieDBScreen}
   />,
   <Project key="p4" name="COOCKBOOK" class="projectU" img={cookbookDBScreen} />,
-  <Project key="p5" name="CHATAPP" class="project" img={chatAppScreen} />,
-  <Project key="p6" name="API" class="projectU" img={restaScreenshot} />,
+  <Project
+    key="p5"
+    name="CHATAPP"
+    class="project"
+    img={chatAppScreen}
+    code="https://github.com/RomanZujev/chat-app"
+  />,
+  <Project
+    key="p6"
+    name="API WEATHER"
+    class="project"
+    img={weatherMap}
+    code="https://github.com/RomanZujev/weathermap"
+  />,
 ];
 
 export default function Main() {
