1e6d908a49f13ae5e43a01c4fe6c8c7b275b7cc2
RomanZujev <roman.zujev@gmail.com>
2022-03-21 19:36:09 +0200

    added onClick page open event

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,13 @@
 export default function Project(props) {
+  function openNewTab() {
+    window.open(props.code, "_blank");
+  }
   return (
     <div className={props.class}>
       <img src={props.img} alt={props.name + " img"} />
       <div className="projectTitle">{props.name}</div>
       <div className="buttons">
-        <button>CODE</button>
+        <button onClick={openNewTab}>CODE</button>
         <button>PROJECT</button>
       </div>
     </div>
