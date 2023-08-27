a36d9a2905a73ea91a0f00072e2e7906e86c42a6
Roman <roman.zujev@gmail.com>
2023-08-27 22:20:46 +0300

    implemented the initial objects placed in a circle

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,11 +1,46 @@
+//import { useThree } from "@react-three/fiber";
+
 function Experience() {
+  // const three = useThree();
+  // three.camera.rotateY(-Math.PI * 0.11);
+  //(x, y) = (r * cos(θ), r * sin(θ))
+  const radius = 11;
+  const numberOfElements = 16;
+  //const sectorUnits = 360 / numberOfElements;
+  //  ^ which in radians is '2 * Math.PI /numberOfElements'
+  const radians = (2 * Math.PI) / numberOfElements;
+
+  const boxArray = [];
+  for (let i = 1; i <= numberOfElements; i++) {
+    const positionx = radius * Math.cos(radians * i);
+    const positiony = radius * Math.sin(radians * i);
+    const rotation = radians * i;
+    boxArray.push(
+      <mesh position-x={positionx} position-y={positiony} rotation-z={rotation}>
+        <boxGeometry args={[0.01, 3, 4]} />
+        <meshPhongMaterial
+          opacity={i === 1 ? 1 : 0.2}
+          transparent
+          color={i === 1 ? "#cfac33" : "rgb(92, 88, 92)"}
+          // side={THREE.DoubleSide}
+        />
+      </mesh>
+    );
+  }
+
   return (
     <>
-      <mesh position-x={5} rotation-y={-Math.PI * 0.4}>
-        <planeGeometry args={[2.5, 5]} />
-        <meshBasicMaterial color={"#cfac33"} wireframe />
-      </mesh>
+      <group>
+        <pointLight position={[0, 0, 0]} intensity={2} />
+        {boxArray}
+        <mesh scale={100}>
+          <planeGeometry />
+          <meshStandardMaterial color="greenyellow" />
+        </mesh>
+      </group>
     </>
   );
 }
 export default Experience;
+
+//<boxGeometry args={[0.01, 3, 4]} />
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,21 @@
 import { Canvas } from "@react-three/fiber";
+
 import Experience from "./Experience";
 import Overlay from "./Overlay";
 export default function Project(props) {
   return (
     <div id="projectsContainer">
       <Overlay />
-      <Canvas>
+      <Canvas
+        camera={{
+          zoom: 2,
+          fov: 45,
+          position: [0, -2, 0],
+        }}
+      >
         <Experience></Experience>
       </Canvas>
     </div>
   );
 }
+//<Canvas camera={{ zoom: 2, fov: 45, position: [0, -2, 0]}}>
