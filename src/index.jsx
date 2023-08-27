bc79919fa8c3de7b097641be579711bf3d7b6655
Roman <roman.zujev@gmail.com>
2023-08-27 23:50:42 +0300

    newer version of API, old one broke Drei

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App key="app" />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <App />
+  </React.StrictMode>
 );
